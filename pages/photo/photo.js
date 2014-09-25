481ca9fe59044391483bdfd3f861f18325c5e578
chestozo <chestozo@yandex-team.ru>
2014-09-26 02:36:23 +0400

    new photos

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -48,7 +48,10 @@ ns.View.define('photo-preview', {
 var photos = ns.Model.get('photos').setData({ images: { image: [] } });
 
 photos.insert([
-    ns.Model.get('photo', { 'image-id': 1 }).setData({ id: 1, url_: 'http://img-fotki.yandex.ru/get/4522/111182131.5/0_6358f_a0da1182_' }),
-    ns.Model.get('photo', { 'image-id': 2 }).setData({ id: 2, url_: 'http://img-fotki.yandex.ru/get/4417/31916371.16/0_5d295_d72044a2_' }),
-    ns.Model.get('photo', { 'image-id': 3 }).setData({ id: 3, url_: 'http://img-fotki.yandex.ru/get/4412/47303295.18/0_192ee2_9293c321_' })
+    ns.Model.get('photo', { 'image-id': 4 }).setData({ id: 4, url_: 'http://img-fotki.yandex.ru/get/9329/27316011.2/0_daca2_13cf7df8_' }),
+    ns.Model.get('photo', { 'image-id': 5 }).setData({ id: 5, url_: 'http://img-fotki.yandex.ru/get/4425/22805354.7/0_6114c_aaf2746c_' }),
+    ns.Model.get('photo', { 'image-id': 1 }).setData({ id: 1, url_: 'http://img-fotki.yandex.ru/get/9835/131505588.d6/0_f43b7_53812e6e_' }),
+    ns.Model.get('photo', { 'image-id': 2 }).setData({ id: 2, url_: 'http://img-fotki.yandex.ru/get/6736/10566499.dd/0_9342c_c2f06db9_' }),
+    ns.Model.get('photo', { 'image-id': 3 }).setData({ id: 3, url_: 'http://img-fotki.yandex.ru/get/9759/202825749.e/0_da572_89ef5e92_' }),
+
 ]);
