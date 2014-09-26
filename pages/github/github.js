1896e59ddcd7fe4d733ca89134a5209f25b15228
chestozo <chestozo@yandex-team.ru>
2014-09-26 04:24:19 +0400

    almost there

--- pages/github/github.js
+++ pages/github/github.js
@@ -32,6 +32,33 @@ ns.Model.define('github-profile', {
     }
 });
 
+ns.Model.define('github-issue', {
+    params: { id: null }
+});
+
+ns.Model.define('github-issues', {
+    params: {
+        'login': null
+    },
+    split: {
+        items: '.',
+        model_id: 'github-issue',
+        params: {
+            'id': '.number'
+        }
+    },
+    methods: {
+        request: function() {
+            return ns.http('https://api.github.com/repos/yandex-ui/noscript/issues?creator={login}'.replace('{login}', this.params['login']), {}, { type: 'GET' })
+                .then(function(data) {
+                    this.setData(data);
+                }, function(error) {
+                    this.setError(error);
+                }, this);
+        }
+    }
+});
+
 ns.View.define('github-index', {
     events: {
         'keydown input': 'onKeyDown'
@@ -49,6 +76,14 @@ ns.View.define('github-profile', {
     models: [ 'github-profile' ]
 });
 
-ns.View.define('github-issues', {
-    // models: [ 'github-issues' ]
+ns.View.define('github-issue', {
+    models: [ 'github-issue' ]
+});
+
+ns.ViewCollection.define('github-issues', {
+    models: [ 'github-issues' ],
+    split: {
+        byModel: 'github-issues',
+        intoViews: 'github-issue'
+    }
 });
